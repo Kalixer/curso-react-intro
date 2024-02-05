3c8ff29481bb2f7a024ac67c739381f6f0adf606
kalixer <al.caceres.ag72@gmail.com>
2024-02-05 16:37:45 -0300

    [Clase 7]

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,16 +1,20 @@
+import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('')
+  console.log(searchValue)
+
+  function EventoFoo(event) {
+    setSearchValue(event.target.value)
+  }
+
   return (
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
-      onChange={(event) => {
-        console.log('Escribiste en el TodoSearch');
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
-      }}
+      value={searchValue}
+      onChange={EventoFoo}
     />
   );
 }
