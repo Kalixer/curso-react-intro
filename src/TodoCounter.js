bf4583e1f5f230fdcdcb74b474ce378bc7b00bc7
kalixer <al.caceres.ag72@gmail.com>
2024-02-07 15:37:26 -0300

    [Challenge finished] El h1 de TodoCounter.js muestra un mensaje u otro en función si todos los TODOs están completados o no

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,11 +1,20 @@
 import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
-  return (
-    <h1 className="TodoCounter">
-      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
-    </h1>
-  );
+  
+  if (completed != total) {
+    return (
+      <h1 className="TodoCounter">
+        Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+      </h1>
+    )
+  } else {
+    return (
+      <h1 className="TodoCounter">
+        🎉🎉 Felicidades, haz completado todos los TODOs 🎉🎉
+      </h1>
+    )
+  }
 }
 
 export { TodoCounter };
